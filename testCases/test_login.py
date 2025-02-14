746288929149d6f1ab86ed8473b8fe8ef3985e11
PrajwalKPaple <papleprajwal1999@gmail.com>
2025-02-14 15:43:41 +0530

    Update test_login.py

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -10,6 +10,7 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from utilities.customLogger import LogGen
 
+// This is for oonly testing purpose
 
 class Test_001_Login:
     baseUrl = ReadConfig.getApplicationURL()
